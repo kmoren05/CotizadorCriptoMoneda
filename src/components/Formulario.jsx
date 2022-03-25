ce1b2e70b54d6e4f11469db98cbbe05402d75794
kmoren05 <91289212+kmoren05@users.noreply.github.com>
2022-03-25 12:22:06 -0500

    Añadiendo state y retornando valor

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import useSelectMonedas from '../hooks/useSelectMonedas'
+import { monedas } from '../data/monedas'
+
+const InputSubmit = styled.input`
+background-color:#9497FF;
+border: none;
+width: 100%;
+padding: 10px;
+color: #fff;
+font-weight:700;
+text-transform:uppercase;
+font-size:20px;
+border-radius:5px;
+transition: background-color .3s ease;
+
+&::hover{
+
+    background-color:#7A7DFE;
+    cursor:pointer;
+}
+
+
+`
+
+const Formulario = () => {
+
+  
+
+    const [SelectMonedas] = useSelectMonedas('Elige tu moneda', monedas)
+ 
+
+ 
+
+  return (
+    <form>
+    
+         <SelectMonedas />
+        
+           <InputSubmit
+            type="submit" 
+             value="Cotizar"/>
+
+    </form>
+  )
+}
+
+export default Formulario
---
+++ src/data/monedas.js
@@ -0,0 +1,12 @@
+const monedas=[
+    {id:'USD',nombre:'Dolar estados Unidos'},
+    {id:'MXN',nombre:'Pesos Mexicanos'},
+    {id:'EUR',nombre:'Euro'},
+    {id:'COP',nombre:'Pesos Colombianos'},
+    {id:'COP',nombre:'Libre esterlina'},
+
+]
+ export {
+
+    monedas
+ }
